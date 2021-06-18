5022c274fc9a3f7fb7e16fb07f3365f573dd5175
Kotorem_sama <rickyman2002.rick@gmail.com>
2021-06-18 17:23:24 +0200

    sss

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,9 +4,40 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
-            
+            int[][] jagged = new int[][]{
+            new int[]{2, 3},
+            new int[]{7, 8, 9},
+            new int[]{4}
+            };
+            string s = Method1D(jagged[1], 1);
+            s = Method2D(jagged, 0, 0);
+        }
+
+        public static string Method1D(int[] arr, int i)
+        {
+            if (i >= arr.Length)
+            {
+                return "";
+            }
+            return arr[i] + Method1D(arr, i + 1);
+        }
+
+        public static string Method2D(int[][] arr, int i, int j)
+        {
+            if (i >= arr.Length)
+            {
+                return "";
+            }
+            if (j < arr[i].Length)
+            {
+                return arr[i][j] + Method2D(arr, i, j + 1);
+            }
+            else
+            {
+                return "\n" + Method2D(arr, i + 1, 0);
+            }
         }
     }
 }
