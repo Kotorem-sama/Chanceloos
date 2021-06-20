0f35329dc24f39700560f96dba32493f34e8981f
Kotorem_sama <rickyman2002.rick@gmail.com>
2021-06-20 19:51:25 +0200

    rgtv

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,32 +4,6 @@ namespace ConsoleApp2
 {
     class Program
     {
-        public static string[] Push(string[] aras, string addd)
-        {
-            string[] newara = new string[aras.Length + 1];
-            for (int i = 0; i < aras.Length; i++)
-            {
-                newara[i] = aras[i];
-            }
-            newara[aras.Length] = addd;
-            return newara;
-        }
-
-        public static string Peek(string[] ara)
-        {
-            return ara[ara.Length - 1];
-        }
-
-        public static Tuple<string[], string> Pop(string[] aras)
-        {
-            string[] newara = new string[aras.Length - 1];
-            for (int i = 0; i < aras.Length - 1; i++)
-            {
-                newara[i] = aras[i];
-            }
-            return Tuple.Create(newara, aras[aras.Length - 1]);
-        }
-
         public static bool AreSameSize(int[][] arr1, int[][] arr2)
         {
             int count1 = 0;
@@ -53,6 +27,25 @@ namespace ConsoleApp2
             {
                 return false;
             }
+        }
+
+        public static int[][] AddArrays(int[][] arr1, int[][] arr2)
+        {
+            bool check = AreSameSize(arr1, arr2);
+            if (check)
+            {
+                int[][] ret = new int[arr1.Length][];
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    ret[i] = new int[arr1[i].Length];
+                    for (int j = 0; j < arr1[i].Length; j++)
+                    {
+                        ret[i][j] = arr1[i][j] + arr2[i][j];
+                    }
+                }
+                return ret;
+            }
+            return null;
         }
     }
 }
